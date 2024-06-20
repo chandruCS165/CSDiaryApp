e05003910d1d4bbf261505ec83f08bf20c2c8549
Subramaniam <chandrusubavasu@gmail.com>
2024-06-20 19:45:31 +0530

    Final Commit

---
+++ app/src/main/java/com/example/csdiaryapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.csdiaryapp.util
+
+object Constants {
+    const val APP_ID = "mycsdiaryapp-vtjrcsz"
+    const val CLIENT_ID = "676262903640-8tiarqgpv11bs446lkdf187651jf636q.apps.googleusercontent.com"
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
+}
